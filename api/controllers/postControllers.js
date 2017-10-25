dd6644e1849bf80d914e9a473453f52dddc380cf
Donnie Reese <donniereese@gmail.com>
2017-10-25 08:28:21 -0500

    Implemented post.getPosts & post.newPost controllers.

--- api/controllers/postControllers.js
+++ api/controllers/postControllers.js
@@ -8,21 +8,46 @@ const STATUS_USER_ERROR = 422;
 
 // controller description
 const getPosts = (req, res) => {
-    res.json(false);
+    Post.find({}).exec((err, p) => {
+        if (err) return res.status(STATUS_USER_ERROR).json({ stack: err.stack, message: err.message });
+
+        res.json(p);
+    });
 };
 
 // controller description
 const newPost = (req, res) => {
+    console.log('newPost: ', req.body);
+    // Gather post-related input
+    const { title, content } = req.body;
+    // check post input
+    if (!title || !content) return res.status(STATUS_USER_ERROR).json({ error: 'Some required post information not supplied.' });
+    // Make a new Post document
+    const newPost = new Post({ title, content });
+    // Save it or lose it
+    newPost.save()
+    .then((p) => {
+        // Check if a new post was not created
+        if (p === null || p === {}) return res.status(STATUS_USER_ERROR).json({ error: 'Post save returned no new post.' });
+
+        res.json(p);
+    })
+    .catch((err) => {
+        // If there is an error
+        return res.status(STATUS_USER_ERROR).json({ stack: saveErr.stack, message: saveErr.message });
+    });
     res.json(false);
 };
 
 // controller description
 const getById = (req, res) => {
+    console.log('getById: ', req.body);
     res.json(false);
 };
 
 // controller description
 const updateById = (req, res) => {
+    console.log('updateById: ', req.body);
     res.json(false);
 };
 
